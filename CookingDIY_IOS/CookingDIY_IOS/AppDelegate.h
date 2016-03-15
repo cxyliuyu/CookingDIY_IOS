2365cb413a95bc2acf538ca97be0f9225f1acdb4
刘宇 <admin@admindemacbook-pro.local>
2016-03-15 23:34:16 +0800

    Inint

---
+++ CookingDIY_IOS/CookingDIY_IOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CookingDIY_IOS
+//
+//  Created by admin on 16/3/15.
+//  Copyright © 2016年 cxyliuyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CookingDIY_IOS/CookingDIY_IOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CookingDIY_IOS
+//
+//  Created by admin on 16/3/15.
+//  Copyright © 2016年 cxyliuyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
