d204cb59ee3ce66fa0ea889b118b7e8fce68c6db
cxyliuyu <1020093037@qq.com>
2016-03-19 21:32:36 +0800

    test

--- CookingDIY_IOS/CookingDIY_IOS/AlarmViewController.m
+++ CookingDIY_IOS/CookingDIY_IOS/AlarmViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     super.view.backgroundColor = [UIColor yellowColor];
+    NSLog(@"打印东西");
 }
 
 - (void)didReceiveMemoryWarning {
--- CookingDIY_IOS/CookingDIY_IOS/main.m
+++ CookingDIY_IOS/CookingDIY_IOS/main.m
@@ -5,7 +5,6 @@
 //  Created by admin on 16/3/15.
 //  Copyright © 2016年 cxyliuyu. All rights reserved.
 //
-
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
